72835d6db1738eaf54525a5fb967438f78c7906f
Cheerego7 <meetcheerego@gmail.com>
2019-05-19 17:50:53 +0800

    test

--- data/web/index.php
+++ data/web/index.php
@@ -20,7 +20,7 @@ catch( Exception $Exception )
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>方糖简历</title>
+    <title>Cheer简历</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="http://lib.sinaapp.com/js/jquery/3.1.0/jquery-3.1.0.min.js"></script>
